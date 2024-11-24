fc24440872ecf5b53aa9806c1e76106984c26a4c
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-24 14:41:49 -0500

    feat(personnel): add national provider identifier value object.

---
+++ src/main/java/org/hign/platform/hartford/personnel/domain/model/valueobjects/NationalProviderIdentifier.java
@@ -0,0 +1,19 @@
+package org.hign.platform.hartford.personnel.domain.model.valueobjects;
+
+import java.util.UUID;
+
+/**
+ * @summary
+ * This class represents the National Provider Identifier (NPI) of a provider.
+ * @param nationalProviderIdentifier The NPI of the provider.
+ */
+public record NationalProviderIdentifier(String nationalProviderIdentifier) {
+    /***
+     * @summary
+     * Constructor for the NationalProviderIdentifier class. It receives a string with the NPI of the provider.
+     * @param nationalProviderIdentifier The NPI of the provider.
+     */
+    public NationalProviderIdentifier(String nationalProviderIdentifier) {
+        this.nationalProviderIdentifier = UUID.fromString(nationalProviderIdentifier).toString();
+    }
+}
